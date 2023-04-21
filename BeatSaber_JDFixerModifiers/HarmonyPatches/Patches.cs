ef5cb8baff71ea1e4fb7ccfa15225cd46817c997
Loloppe <LaurentBlaisCote@outlook.com>
2023-04-21 16:33:46 -0400

    Now work with JDFixer

--- BeatSaber_JDFixerModifiers/HarmonyPatches/Patches.cs
+++ BeatSaber_JDFixerModifiers/HarmonyPatches/Patches.cs
@@ -1,44 +1,63 @@
 ﻿using HarmonyLib;
 using System;
-using System.Reflection;
 
 namespace BeatSaber_JDFixerModifiers.HarmonyPatches
 {
 	[HarmonyPatch(typeof(BeatmapObjectSpawnMovementData), "Init")]
-	[HarmonyPriority(Priority.Low)]
+    [HarmonyAfter(new string[] { "com.zephyr.BeatSaber.JDFixer" })]
 	static class Patches
 	{
 		static void Prefix(ref float noteJumpValue)
 		{
 			if (Configs.Configs.Instance.Enabled)
 			{
-				if (Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings != null)
+				if (Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings != null) // Practice mode
 				{
-					noteJumpValue *= Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings.songSpeedMul;
-				}
-				else
-                {
-					if (!Configs.Configs.Instance.Overwrite)
-					{
-						noteJumpValue *= Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul;
+					if(Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings.songSpeedMul >= 1)
+                    {
+						noteJumpValue += Math.Abs(noteJumpValue - Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings.songSpeedMul * noteJumpValue);
 					}
 					else
+                    {
+						noteJumpValue -= Math.Abs(noteJumpValue - Plugin.levelData.GameplayCoreSceneSetupData.practiceSettings.songSpeedMul * noteJumpValue);
+					}
+				}
+				else // Non-practice
+				{
+					if (!Configs.Configs.Instance.Overwrite) // Custom multiplier
 					{
+						if (Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul >= 1)
+						{
+							noteJumpValue += Math.Abs(noteJumpValue - Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul * noteJumpValue);
+						}
+						else
+						{
+							noteJumpValue -= Math.Abs(noteJumpValue - Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul * noteJumpValue);
+						}
+					}
+					else // Default multiplier
+					{
+						var multi = 1f;
 						if (Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeed == GameplayModifiers.SongSpeed.Slower)
 						{
-							noteJumpValue *= Configs.Configs.Instance.SS;
+							multi = Configs.Configs.Instance.SS;
 						}
 						else if (Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeed == GameplayModifiers.SongSpeed.Faster)
 						{
-							noteJumpValue *= Configs.Configs.Instance.FS;
+							multi = Configs.Configs.Instance.FS;
 						}
 						else if (Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeed == GameplayModifiers.SongSpeed.SuperFast)
 						{
-							noteJumpValue *= Configs.Configs.Instance.SF;
+							multi = Configs.Configs.Instance.SF;
+						}
+
+						if (Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul >= 1)
+						{
+							noteJumpValue += Math.Abs(noteJumpValue - multi * noteJumpValue);
 						}
 						else
-                        {
-							noteJumpValue *= Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.songSpeedMul;
+						{
+							noteJumpValue -= Math.Abs(noteJumpValue - multi * noteJumpValue);
 						}
 					}
 				}
